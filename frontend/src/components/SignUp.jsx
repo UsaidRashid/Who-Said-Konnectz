4c33ae423087927971d0ab4118616aad6816ea59
UsaidRashid <rashidusaid2@gmail.com>
2024-08-18 11:40:38 +0530

    Achieved Signup Responsiveness

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -9,7 +9,6 @@ import {
   Row,
   Col,
 } from "react-bootstrap";
-import "../styles/signup.css";
 const api = import.meta.env.VITE_BACKEND_URL;
 
 export default function SignUp() {
@@ -72,128 +71,122 @@ export default function SignUp() {
   };
 
   return (
-    <div className="signup-container">
-      <Container className="signup-form-container w-50">
-        <h1 className="text-center mb-4 fs-3 bolder">
-          Join our Who-Said Family!
-        </h1>
-        <Form onSubmit={handleSubmit} className="mx-5">
-          <Row className="mb-3">
-            <Col>
-              <FloatingLabel
-                controlId="username"
-                label="Username"
-                className="text-black"
-              >
-                <Form.Control
-                  type="text"
-                  placeholder="Username"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                  required
-                />
-              </FloatingLabel>
-            </Col>
-          </Row>
-          <Row className="mb-3">
-            <Col>
-              <FloatingLabel
-                controlId="email"
-                label="Email Address"
-                className="text-black"
-              >
-                <Form.Control
-                  type="email"
-                  placeholder="Email Address"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                />
-              </FloatingLabel>
-            </Col>
-          </Row>
-          <Row className="mb-3">
-            <Col>
-              <FloatingLabel
-                controlId="name"
-                label="Name"
-                className="text-black"
-              >
-                <Form.Control
-                  type="text"
-                  placeholder="Name"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  required
-                />
-              </FloatingLabel>
-            </Col>
-          </Row>
-          <Row className="mb-3">
-            <Col>
-              <FloatingLabel
-                controlId="contact"
-                label="Contact Number"
-                className="text-black"
-              >
-                <Form.Control
-                  type="text"
-                  placeholder="Contact Number"
-                  value={contact}
-                  onChange={(e) => setContact(e.target.value)}
-                  required
-                />
-              </FloatingLabel>
-            </Col>
-          </Row>
-          <Row className="mb-3">
-            <Col>
-              <FloatingLabel
-                controlId="password"
-                label="Password"
-                className="text-black"
-              >
-                <Form.Control
-                  type="password"
-                  placeholder="Password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                />
-              </FloatingLabel>
-            </Col>
-          </Row>
-          <Row className="mb-3">
-            <Col>
-              <Form.Group controlId="profilePic">
-                <label
-                  htmlFor="file-upload"
-                  className="cursor-pointer inline-flex items-center px-4 py-2 bg-emerald-600 text-white font-semibold rounded-lg shadow-md hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-emerald-500"
-                >
-                  Upload a cheesy Profile Picture!
-                  <input
-                    id="file-upload"
-                    type="file"
-                    className="hidden"
-                    name="profilePic"
-                    onChange={handleFileChange}
+    <>
+      <div className="d-flex flex-column min-vh-100 bg-gradient-to-r from-emerald-500 to-emerald-700 text-white justify-content-center align-items-center">
+        <Container
+          className="bg-white rounded-3 shadow p-4"
+          style={{ maxWidth: "500px" }}
+        >
+          <h1 className="text-center mb-4 text-emerald-600 font-bold fs-3">
+            Join our Who-Said Family!
+          </h1>
+          <Form onSubmit={handleSubmit} className="mx-5">
+            <Row className="mb-3">
+              <Col>
+                <FloatingLabel controlId="username" label="Username">
+                  <Form.Control
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                    className="text-white bg-black"
                   />
-                </label>
-              </Form.Group>
-            </Col>
-          </Row>
-
-          <Button type="submit" variant="primary" className="w-50 mb-3">
-            Sign Up
-          </Button>
-          <p className="text-center text-black">
-            Already have an account? <Link to="/login">Sign in</Link>
-          </p>
-        </Form>
-      </Container>
-      <footer className="signup-footer">
-        <p>Copyright © Who-Said Private Limited {new Date().getFullYear()}</p>
+                </FloatingLabel>
+              </Col>
+            </Row>
+            <Row className="mb-3">
+              <Col>
+                <FloatingLabel controlId="email" label="Email Address">
+                  <Form.Control
+                    type="email"
+                    placeholder="Email Address"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    className="text-white bg-black"
+                  />
+                </FloatingLabel>
+              </Col>
+            </Row>
+            <Row className="mb-3">
+              <Col>
+                <FloatingLabel controlId="name" label="Name">
+                  <Form.Control
+                    type="text"
+                    placeholder="Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                    className="text-white bg-black"
+                  />
+                </FloatingLabel>
+              </Col>
+            </Row>
+            <Row className="mb-3">
+              <Col>
+                <FloatingLabel controlId="contact" label="Contact Number">
+                  <Form.Control
+                    type="text"
+                    placeholder="Contact Number"
+                    value={contact}
+                    onChange={(e) => setContact(e.target.value)}
+                    required
+                    className="text-white bg-black"
+                  />
+                </FloatingLabel>
+              </Col>
+            </Row>
+            <Row className="mb-3">
+              <Col>
+                <FloatingLabel controlId="password" label="Password">
+                  <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    className="text-white bg-black"
+                  />
+                </FloatingLabel>
+              </Col>
+            </Row>
+            <Row className="mb-3">
+              <Col>
+                <Form.Group controlId="profilePic">
+                  <label
+                    htmlFor="file-upload"
+                    className="cursor-pointer inline-flex items-center px-4 py-2 bg-emerald-600 text-white font-semibold rounded-lg shadow-md hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-emerald-500"
+                  >
+                    Upload a cheesy Profile Picture!
+                    <input
+                      id="file-upload"
+                      type="file"
+                      className="hidden"
+                      name="profilePic"
+                      onChange={handleFileChange}
+                    />
+                  </label>
+                </Form.Group>
+              </Col>
+            </Row>
+            <Button type="submit" variant="primary" className="w-100 mb-3">
+              Sign Up
+            </Button>
+            <p className="text-center text-black">
+              Already have an account?{" "}
+              <Link to="/login" className="text-primary">
+                Sign in
+              </Link>
+            </p>
+          </Form>
+        </Container>
+      </div>
+      <footer className="text-center bg-white py-3 text-dark w-100 mt-3">
+        <p className="mb-0">
+          Copyright © Who-Said Private Limited {new Date().getFullYear()}
+        </p>
       </footer>
-    </div>
+    </>
   );
 }
