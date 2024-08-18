f3f25c2b4468509c5902649e668b9dc2da1ceb43
UsaidRashid <rashidusaid2@gmail.com>
2024-08-18 13:33:53 +0530

    Profile Update

--- frontend/src/components/ProfileUpdate.jsx
+++ frontend/src/components/ProfileUpdate.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
-// import "../styles/updateProfile.css";
 const api = import.meta.env.VITE_BACKEND_URL;
 
 export default function ProfileUpdate() {
@@ -75,47 +74,54 @@ export default function ProfileUpdate() {
   };
 
   return (
-    <div className="profile-update-container mt-10">
-      <div className="form-container">
-        <h3 className="text-center text-dark font-weight-bold fs-4 mb-4">
+    <div className="container mt-32">
+      <div
+        className="bg-white rounded-lg shadow-lg p-4 mx-auto"
+        style={{
+          maxWidth: "600px",
+          minHeight: "100vh",
+          backgroundColor: "#d4edda",
+        }}
+      >
+        <h3 className="text-center text-emerald-900 font-weight-bold mb-4 text-4xl">
           Update your profile!
         </h3>
         <form onSubmit={handleSubmit}>
-          <div className="form-group mb-3">
-            <label className="text-dark font-weight-bold fs-5">Name</label>
+          <div className="mb-3">
+            <label className="form-label text-dark fw-bold fs-5">Name</label>
             <input
               type="text"
               name="name"
-              className="form-control"
+              className="form-control border border-gray-300 rounded px-3 py-2"
               value={formData.name}
               onChange={handleChange}
             />
           </div>
-          <div className="form-group mb-3">
-            <label className="text-dark font-weight-bold fs-5">
+          <div className="mb-3">
+            <label className="form-label text-dark fw-bold fs-5">
               Email Address
             </label>
             <input
               type="email"
               name="email"
-              className="form-control"
+              className="form-control border border-gray-300 rounded px-3 py-2"
               value={formData.email}
               onChange={handleChange}
             />
           </div>
-          <div className="form-group mb-3">
-            <label className="text-dark font-weight-bold fs-5">
+          <div className="mb-3">
+            <label className="form-label text-dark fw-bold fs-5">
               Contact No
             </label>
             <input
               type="number"
               name="contact"
-              className="form-control"
+              className="form-control border border-gray-300 rounded px-3 py-2"
               value={formData.contact}
               onChange={handleChange}
             />
           </div>
-          <div className="form-group mb-3">
+          <div className="mb-3">
             <label
               htmlFor="file-upload"
               className="cursor-pointer inline-flex items-center px-4 py-2 bg-emerald-600 text-white font-semibold rounded-lg shadow-md hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-emerald-500"
